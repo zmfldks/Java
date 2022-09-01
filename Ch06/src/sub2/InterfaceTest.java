a98330a3407b717d4508409b5f3a83f8b77afc8e
wanggeuni37 <wanggeuni37@gmail.com>
2022-09-01 16:58:57 +0900

    2022/09/01

--- Ch06/src/sub2/InterfaceTest.java
+++ Ch06/src/sub2/InterfaceTest.java
@@ -4,6 +4,9 @@ package sub2;
  * 이름 : 이왕근
  * 내용 : 인터페이스 실습하기
  * 
+ * 인터페이스(Interface)
+ * - 클래스간 공통의 표준 규격을 통해 클래스 구조를 설계하는 문법
+ * - 인터페이스를 활용해서 객체간 결합도를 완화하는 효과(다형성)
  */
 public class InterfaceTest {
 	public static void main(String[] args) {
@@ -21,6 +24,20 @@ public class InterfaceTest {
 		
 		
 		// 인터페이스 결합도 완화
+		Bulb bulb = new Bulb();
+		Socket socket = new Cable(bulb);
+		
+		socket.switchOn();
+		socket.switchOff();
+		
+		// 인터페이스 다중 상속 효과
+		Tv smartTv = new Tv();
+		smartTv.powerOn();
+		smartTv.booting();
+		smartTv.browser();
+		smartTv.access();
+		smartTv.powerOff();
+		
 		
 		
 		
@@ -28,3 +45,40 @@ public class InterfaceTest {
 		
 	}
 }
+
+class Tv extends Computer implements Internet {
+
+	@Override
+	public void access() {
+		System.out.println("인터넷 접속...");
+		
+	}
+	public void powerOn() {
+		System.out.println("전원 켬...");
+	}
+	
+	public void powerOff() {
+		System.out.println("전원 끔...");
+	}
+	
+}
+
+class Computer {
+	
+	public void booting() {
+		System.out.println("시스템 부팅...");
+	}
+	public void browser() {
+		System.out.println("브라우저 실행...");
+	}
+}
+
+interface Internet {
+	public void access();
+}
+
+
+
+
+
+
---
+++ Ch06/src/sub2/Socket.java
@@ -0,0 +1,9 @@
+package sub2;
+
+public interface Socket {
+
+	public void switchOn();
+	public void switchOff();
+	
+	
+}
