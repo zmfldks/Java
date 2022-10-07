ba55be16b4221582122551eaa68d2e836f76fa28
wanggeuni37 <wanggeuni37@gmail.com>
2022-10-07 19:48:47 +0900

    2022/10/07

--- Book1/src/Ch05/p148.java
+++ Book1/src/Ch05/p148.java
@@ -2,8 +2,15 @@ package Ch05;
 /*
  * 날짜 : 2022/09/22
  * 이름 : 이왕근
- * 내용 :
+ * 내용 : 인스턴스 여러 개 생성하기
  */
 public class p148 {
-
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.studentName = "안연수";
+		System.out.println(student1.getStudentName());
+		Student student2 = new Student();
+		student2.studentName = "안승연";
+		System.out.println(student2.getStudentName());
+	}
 }
--- Book1/src/Ch05/p150.java
+++ Book1/src/Ch05/p150.java
@@ -2,8 +2,17 @@ package Ch05;
 /*
  * 날짜 : 2022/09/22
  * 이름 : 이왕근
- * 내용 :
+ * 내용 : 참조 값 출력하기
  */
 public class p150 {
-
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.studentName = "안연수";
+		
+		Student student2 =new Student();
+		student2.studentName = "안승연";
+		
+		System.out.println(student1);
+		System.out.println(student2);
+	}
 }
