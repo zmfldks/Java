c4182366bd73f310480272bc12e965e4fe3e9f5b
wanggeuni37 <wanggeuni37@gmail.com>
2022-11-02 19:50:43 +0900

    2022/11/02

---
+++ Book1/src/Ch05/Person.java
@@ -0,0 +1,21 @@
+package Ch05;
+/*
+ * 날짜 : 2022/09/22
+ * 이름 : 이왕근
+ * 내용 :
+ */
+public class Person {
+	String name;
+	float height;
+	float weight;
+	
+	
+	public Person(String pname) {
+		name = pname;
+	}
+
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ Book1/src/Ch05/PersonTest.java
@@ -0,0 +1,12 @@
+package Ch05;
+/*
+ * 날짜 : 2022/09/22
+ * 이름 : 이왕근
+ * 내용 :
+ */
+public class PersonTest {
+	public static void main(String[] args) {
+		Person personLee = new Person();
+		
+	}
+}
